1d4929b72bc97037a868aeb4ca4c1a04232e8403
Thalles <thallesrafaell@outlook.com>
2024-02-09 01:07:01 -0300

    feat: JPA repository, dependency injection, database seeding

---
+++ src/main/java/dev/thallesrafael/apicurso/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package dev.thallesrafael.apicurso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.thallesrafael.apicurso.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    
+}
