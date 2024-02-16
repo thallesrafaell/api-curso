135389799556e4c3da8c3e2e4aa2eb57b3c64367
Thalles <thallesrafaell@outlook.com>
2024-02-15 22:18:28 -0300

    feat: Payment onetone association

--- src/main/java/dev/thallesrafael/apicurso/config/TestConfig.java
+++ src/main/java/dev/thallesrafael/apicurso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import dev.thallesrafael.apicurso.entities.Category;
 import dev.thallesrafael.apicurso.entities.Order;
 import dev.thallesrafael.apicurso.entities.OrderItem;
+import dev.thallesrafael.apicurso.entities.Payment;
 import dev.thallesrafael.apicurso.entities.Product;
 import dev.thallesrafael.apicurso.entities.User;
 import dev.thallesrafael.apicurso.entities.enuns.OrderStatus;
@@ -79,6 +80,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
+
 
 
     }
--- src/main/java/dev/thallesrafael/apicurso/entities/Order.java
+++ src/main/java/dev/thallesrafael/apicurso/entities/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import dev.thallesrafael.apicurso.entities.enuns.OrderStatus;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -37,6 +39,12 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+
+    //NO CASO DE MAPEAMENTO ONETOONE COM MESMO ID A GENTE DEVE  COLOCAR O CASCADE COMO ABAIXO
+    @OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+    private Payment payment;
+
+   
 
     public Order() {
     }
@@ -85,6 +93,16 @@ public class Order implements Serializable {
     public Set<OrderItem> getItems(){
         return items;
     }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    
 
     @Override
     public int hashCode() {
