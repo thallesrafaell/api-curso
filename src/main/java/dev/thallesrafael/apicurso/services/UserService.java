8ef6f4ecd40edd69bd323e1f16aad28011564bca
Thalles Rafael <thallesrafaell@outlook.com>
2024-02-17 20:36:20 -0300

    feat: Exception handling - findById

--- src/main/java/dev/thallesrafael/apicurso/services/UserService.java
+++ src/main/java/dev/thallesrafael/apicurso/services/UserService.java
@@ -3,6 +3,7 @@ package dev.thallesrafael.apicurso.services;
 import java.util.List;
 import java.util.Optional;
 
+import dev.thallesrafael.apicurso.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
